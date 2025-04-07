fd71697db996e84b14736508e83f522df57bd598
Izabelly25 <163589276+Izabelly25@users.noreply.github.com>
2025-04-07 14:31:56 -0300

    Add files via upload
    Alterações no script.js ( 07/03)

---
+++ Iza /Alura/script.js
@@ -0,0 +1,14 @@
+document.addEventListener ('DOMContentLoaded',function(){
+    const aumentaFonteBotao = document.getElementById ('aumenta-fonte');
+    const dimunuiFonteBotao = document.getElementById ('diminui-fonte');
+ let tamanhoAtualFonte=1;
+ aumentaFonteBotao.addEventListener ('click', function (){
+ tamanhoAtualFonte +=0.1;
+   document.body.style.fontSize= '$tamanhoAtualFonte}rem';
+});
+  diminuiFonteBotao.addEventListener ('click', function (){
+  tamanhoAtualFonte-= 0.1;
+  document.body.style.fontSize= '$tamanhoAtualFonte}rem';
+
+  });
+});
